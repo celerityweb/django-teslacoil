cfcc24cb23760b38fd3601534a6707cb48c6d13d
Joshua "jag" Ginsberg <jag@flowtheory.net>
2014-01-25 10:35:33 -0500

    Base python packaging

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+import teslacoil
+
+setup(
+    name='django-teslacoil',
+    version=teslacoil.__version__,
+    packages=find_packages(),
+    url='https://github.com/celerityweb/django-teslacoil/',
+    license='LGPL v3 - see LICENSE file',
+    author='See AUTHORS file',
+    description='TeslaCoil is a Django application to decouple the stock '
+                'django.contrib.admin site into ReSTful APIs and to build '
+                'UX-rich clients using popular Javascript MVC framework'
+)
