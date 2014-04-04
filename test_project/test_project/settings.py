7d0a7bc55309ce72d588e8541e01bc8d80b1791c
Matt Caldwell <matt.caldwell@gmail.com>
2014-04-04 17:23:18 -0400

    add django_extensions

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -121,6 +121,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.admindocs',
+    'django_extensions',
     'rest_framework',
     'teslacoil',
 )
