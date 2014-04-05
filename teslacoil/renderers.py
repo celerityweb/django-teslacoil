6158e047a73d74d9dd1c2f62f395237f9b6d0c7d
Matt Caldwell <matt.caldwell@gmail.com>
2014-04-05 12:00:09 -0400

    remove TeslaEncoder for now... this is being handled by django-jsonschema

--- teslacoil/renderers.py
+++ teslacoil/renderers.py
@@ -1,11 +1,8 @@
 from djangojsonschema.jsonschema import DjangoFormToJSONSchema
 from rest_framework import renderers
-from teslacoil.encoders import TeslaEncoder
 
 
 class TeslaRenderer(renderers.JSONRenderer):
-
-    encoder_class = TeslaEncoder
 
     def render(self, data, accepted_media_type=None, renderer_context=None):
         model_admin = renderer_context['view'].model_admin
