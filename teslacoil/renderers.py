f64ac6cfd83b9d464b599ee2286ae50f1834c50f
Matt Caldwell <matt.caldwell@gmail.com>
2014-04-05 00:40:21 -0400

    begin adding metadata such as field info and model name

--- teslacoil/renderers.py
+++ teslacoil/renderers.py
@@ -1,8 +1,20 @@
 from rest_framework import renderers
+from teslacoil.encoders import TeslaEncoder
+
 
 class TeslaRenderer(renderers.JSONRenderer):
 
+    encoder_class = TeslaEncoder
+
     def render(self, data, accepted_media_type=None, renderer_context=None):
-        response = super(TeslaRenderer, self).render(
-            data, accepted_media_type, renderer_context)
-        return response
+        model = renderer_context['view'].model
+        model_admin = renderer_context['view'].model_admin
+        request = renderer_context['request']
+
+        response_wrapper = {
+            'meta': model_admin,
+            'objects': data,
+        }
+
+        return super(TeslaRenderer, self).render(
+            response_wrapper, accepted_media_type, renderer_context)
